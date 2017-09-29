e3fa13fdb5ea58a00f3177bc13a9188b36823abd
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-09-29 13:02:48 +0300

    add firebase

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
   Name: DS.store(),
   Cuisine: DS.store(),
   Locale: DS.store(),
-  Sitting Capactiy: DS.store()
+  Sitting: DS.store()
 
 });
--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,12 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyAXVdWcF8xQ2qVhv0yDq4jvrtHv4EE5-BE",
+      authDomain: "rate-gate.firebaseapp.com",
+      databaseURL: "https://rate-gate.firebaseio.com",
+      storageBucket: "rate-gate.appspot.com",
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
