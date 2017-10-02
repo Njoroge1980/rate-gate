a82e0f5c99ccb426bede3bb1d0b89037846eb7d6
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-10-02 09:21:05 +0300

    add the link to the dynamic route

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('restaurant')
+  },
+  actions: {
+    saveRestaurant(params) {
+      var newRestaurant = this.store.createRecord('restaurant', params);
+      newRestaurant.save();
+      this.transitionTo('index');
+    }
   }
-  // model() {
-  //   return this.store.findAll('restaurant')
 });
