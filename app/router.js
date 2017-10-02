4d902515ad10c32a050c4766a660f3ecc04e557c
Njoroge Mungai <njorogemungai1980@gmail.com>
2017-10-02 09:20:12 +0300

    add a dynamic route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('restaurant');
+  this.route('restaurant', {
+    path: '/restaurant/:restaurant_id'
+  });
 });
 
 export default Router;
